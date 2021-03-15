4903728ba23d2ce28db8335d575cec810912f812
Marc Schneider <marc.schneider@appgate.com>
2021-03-15 11:35:33 +0100

    missleading and wrong naming of variables

--- desktop.js
+++ desktop.js
@@ -1,4 +1,5 @@
-*  The script assumes a claim called claims.user.desktopsFromAd, 
+/**
+ * The script assumes a claim called claims.user.desktopsFromAd, 
 *  an array type.
 *
 *  1. Map AD attribute <defined in your AD> {multiple values} --> claims.user.desktopsFromAd []string
@@ -9,80 +10,83 @@
 */
 
 
-
 // Helper vars ----------------------------------
-// Note on performance: do not set debug log to true in production. Also, keep the auditlog as 
+// Note on performance: do not set debugLog log to true in production. Also, keep the auditlog as 
 // spares as possible.
-var auditLog = false; // prints audit messages
-var debugLog = false; //prints debug messages
+var audit = false; // prints audit messages
+var debug = false; //prints debugLog messages
 var dryRun = false; // run without any available claim.
 var logPrefix = "rdp-script";
 var hostList = [];
 
 // Utils ----------------------------------------
-function log(msg){
-    if(audit){
-      auditLog(prefix+": "+ msg);
+function log(msg) {
+    if (audit) {
+        auditLog(prefix + ": " + msg);
     }
-  }
+}
+
+
 // log to console log
-function debug(msg){
-    console.log("debug: "+ msg);
+function debugLog(msg) {
+    if (debug) {
+        console.log("debugLog: " + msg);
+    }
 }
 
 // Script vars ----------------------------------
 var domainSuffix = ".appgate.lab";
-var rdpPlatforms = ["Desktops", 
-                    "Developer",
-                    "Laptops",
-                    "Tablets",
-                    "Virtual"];
+var rdpPlatforms = ["Desktops",
+    "Developer",
+    "Laptops",
+    "Tablets",
+    "Virtual"
+];
 var input;
-if (dryRun){
+if (dryRun) {
     var fakeClaim = ""
     input = fakeClaim;
 } else {
-    if (!claims.user.desktopsFromAd){
+    if (!claims.user.desktopsFromAd) {
         // claim is not defined
         return []
     }
     // dumb test if an instantiated array
     try {
         var test = claims.user.desktopsFromAd[0]
-    }
-    catch(err){
+    } catch (err) {
         return []
     }
     input = claims.user.desktopsFromAd;
-}                    
-debug("input -->" + input);
+}
+debugLog("input -->" + input);
 
 
 /// Logic
 var candidates = []
 // select the ones which are in rdpPlatforms
-for (var k = 0; k < input.length; k++){
+for (var k = 0; k < input.length; k++) {
     // is in platform list
-    for (var l = 0; l < rdpPlatforms.length; l++){
-        if (input[k].toLowerCase().indexOf(rdpPlatforms[l].toLowerCase())> -1){
+    for (var l = 0; l < rdpPlatforms.length; l++) {
+        if (input[k].toLowerCase().indexOf(rdpPlatforms[l].toLowerCase()) > -1) {
             candidates.push(input[k])
-            debug("candidate: " + input[k] + ", type: " + rdpPlatforms[l])
-            break
-         }
-    }
-}
-var hostNames = []
-// get the host names, use dumb iteration
-for (var k = 0; k < candidates.length; k++){
-    var t1 = candidates[k].split(",")
-    for (var h = 0; h< t1.length; h++){
-        if (t1[h].indexOf("CN=") > -1) {
-            var hostname = t1[h].split("CN=")[1].replace(",","")
-            hostNames.push(hostname+domainSuffix)
+            debugLog("candidate: " + input[k] + ", type: " + rdpPlatforms[l])
             break
         }
     }
 }
-debug("host names: " + hostNames)
+var hostNames = []
+// get the host names, use dumb iteration
+for (var k = 0; k < candidates.length; k++) {
+    var t1 = candidates[k].split(",")
+    for (var h = 0; h < t1.length; h++) {
+        if (t1[h].indexOf("CN=") > -1) {
+            var hostname = t1[h].split("CN=")[1].replace(",", "")
+            hostNames.push(hostname + domainSuffix)
+            break
+        }
+    }
+}
+debugLog("host names: " + hostNames)
 
 return hostNames
